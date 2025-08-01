c2b5b161dd4775ca4b64b37fb4e9a9ee0eaa3422
Denis <den768653@gmail.com>
2025-08-01 17:57:10 +0300

    [main] Fix plugins

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -5,6 +5,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
   },
   config = function()
+    require("mason").setup()
     require("mason-lspconfig").setup({
       ensure_installed = { "gopls", "ts_ls", "lua_ls", "solargraph" },
       automatic_installation = true,
@@ -12,13 +13,35 @@ return {
 
     local lspconfig = require("lspconfig")
 
-    lspconfig.gopls.setup({})
-    lspconfig.ts_ls.setup({})
-    lspconfig.lua_ls.setup({})
-    lspconfig.solargraph.setup({})
+    -- Функция для keymaps, которая будет вызываться при подключении LSP к буферу
+    local on_attach = function(client, bufnr)
+      local bufopts = { noremap=true, silent=true, buffer=bufnr }
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+      -- Можно добавить другие полезные команды, например:
+      -- vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+      -- vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
+    end
 
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+    -- Настройка каждого LSP сервера
+    lspconfig.gopls.setup({
+      on_attach = on_attach,
+    })
+    lspconfig.ts_ls.setup({
+      on_attach = on_attach,
+    })
+    lspconfig.lua_ls.setup({
+      on_attach = on_attach,
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { "vim" },
+          },
+        },
+      },
+    })
+    lspconfig.solargraph.setup({
+      on_attach = on_attach,
+    })
   end,
 }
-
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,30 +1,29 @@
 return {
-  "jose-elias-alvarez/null-ls.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    local null_ls = require("null-ls")
+	"jose-elias-alvarez/null-ls.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local null_ls = require("null-ls")
 
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.rubocop,
-        null_ls.builtins.formatting.gofmt,
-        null_ls.builtins.formatting.goimports,
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.formatting.rubocop,
+				null_ls.builtins.formatting.gofmt,
+				null_ls.builtins.formatting.goimports,
 
-        null_ls.builtins.diagnostics.rubocop,
-        null_ls.builtins.diagnostics.eslint_d,
-        null_ls.builtins.diagnostics.golangci_lint,
-      },
-    })
+				null_ls.builtins.diagnostics.rubocop,
+				null_ls.builtins.diagnostics.eslint_d,
+				null_ls.builtins.diagnostics.golangci_lint,
+			},
+		})
 
-    vim.keymap.set('n', '<leader>ff', function()
-      vim.lsp.buf.format({ async = true })
-    end, { desc = "Format file" })
+		vim.keymap.set("n", "<leader>ff", function()
+			vim.lsp.buf.format({ async = true })
+		end, { desc = "Format file" })
 
-    vim.keymap.set('n', '<leader>fl', function()
-      vim.diagnostic.open_float()
-    end, { desc = "Show lint errors" })
-  end,
+		vim.keymap.set("n", "<leader>fl", function()
+			vim.diagnostic.open_float()
+		end, { desc = "Show lint errors" })
+	end,
 }
-
